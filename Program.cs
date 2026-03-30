8deefc437739bde6d21e48bc3d74571375e07f8e
Aishwar Pathak <shubhum19@gmail.com>
2026-03-30 22:36:54 +0530

    feat - added method for Adding and Printing Order

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿class Program
+﻿using OrderMatchingEngine.Models;
+using OrderMatchingEngine.Services;
+
+class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Order Matching Engine Started...");
+        var orderBook = new OrderBook();
+
+        orderBook.AddOrder(new Order
+        {
+            Side = OrderSide.Buy,
+            Price = 100,
+            Quantity = 10
+        });
+
+        orderBook.AddOrder(new Order
+        {
+            Side = OrderSide.Sell,
+            Price = 105,
+            Quantity = 5
+        });
+
+        orderBook.Print();
     }
 }
---
+++ Services/OrderBook.cs
@@ -0,0 +1,43 @@
+﻿using OrderMatchingEngine.Models;
+
+namespace OrderMatchingEngine.Services
+{
+    public class OrderBook
+    {
+        private Dictionary<decimal, List<Order>> buyOrders;
+        private Dictionary<decimal, List<Order>> sellOrders;
+
+        public OrderBook()
+        {
+            buyOrders = new Dictionary<decimal, List<Order>>();
+            sellOrders = new Dictionary<decimal, List<Order>>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            var book = order.Side == OrderSide.Buy ? buyOrders : sellOrders;
+            if (!book.ContainsKey(order.Price))
+            {
+                book[order.Price] = new List<Order>();
+            }
+
+            book[order.Price].Add(order);
+            Console.WriteLine("--- ORDER PLACED ------ ");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("BUY ORDERS:");
+            foreach (var kvp in buyOrders)
+            {
+                Console.WriteLine($"Price: {kvp.Key}, Orders: {kvp.Value.Count}");
+            }
+
+            Console.WriteLine("SELL ORDERS:");
+            foreach (var kvp in sellOrders)
+            {
+                Console.WriteLine($"Price:{kvp.Key}, Orders: {kvp.Value.Count}");
+            }
+        }
+    }
+}
