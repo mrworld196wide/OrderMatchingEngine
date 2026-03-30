b7c0eb1b66f7dea36aee652cc09f240739cd7808
Aishwar Pathak <shubhum19@gmail.com>
2026-03-30 21:11:56 +0530

    feat: added Order, Trade models

---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿
+namespace OrderMatchingEngine.Models
+{
+    public class Order
+    {
+        public string Id { get; set; }
+        public OrderSide Side { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+    public enum OrderSide
+    {
+        Buy,
+        Sell
+    }
+}
---
+++ Models/Trade.cs
@@ -0,0 +1,10 @@
+﻿    namespace OrderMatchingEngine.Models
+{
+    public class Trade
+    {
+        public string BuyOrderId { get; set; }
+        public string SellOrderId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
