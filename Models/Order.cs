a65371ec93796fbb0233c427723b7ca228c60e04
Aishwar Pathak <shubhum19@gmail.com>
2026-03-31 19:29:06 +0530

    added default guid and timestamp

--- Models/Order.cs
+++ Models/Order.cs
@@ -3,11 +3,11 @@ namespace OrderMatchingEngine.Models
 {
     public class Order
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public OrderSide Side { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
     public enum OrderSide
     {
