991911ea3e28ace8b6be9bd15d8118a2b9cfd49b
Aishwar Pathak <shubhum19@gmail.com>
2026-03-31 19:31:15 +0530

    sanity check

--- Program.cs
+++ Program.cs
@@ -6,31 +6,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Order Matching Engine Started...");
+        //var orderBook = new OrderBook();
+
+        //orderBook.AddOrder(new Order
+        //{
+        //    Side = OrderSide.Buy,
+        //    Price = 100,
+        //    Quantity = 10
+        //});
+        //orderBook.AddOrder(new Order
+        //{
+        //    Side = OrderSide.Buy,
+        //    Price = 100,
+        //    Quantity = 5
+        //});
+
+        //orderBook.AddOrder(new Order
+        //{
+        //    Side = OrderSide.Sell,
+        //    Price = 105,
+        //    Quantity = 5
+        //});
+
+        //orderBook.Print();
+        //orderBook.GetBestBuy();
+        //orderBook.GetBestSell();
+
         var orderBook = new OrderBook();
+        var engine = new MatchingEngine(orderBook);
 
-        orderBook.AddOrder(new Order
-        {
-            Side = OrderSide.Buy,
-            Price = 100,
-            Quantity = 10
-        });
-        orderBook.AddOrder(new Order
-        {
-            Side = OrderSide.Buy,
-            Price = 100,
-            Quantity = 5
-        });
+        var sellTrades =  engine.ProcessOrder(new Order{ Side = OrderSide.Sell, Price = 95, Quantity = 5});
 
-        orderBook.AddOrder(new Order
-        {
-            Side = OrderSide.Sell,
-            Price = 105,
-            Quantity = 5
-        });
+        var buyTrades = engine.ProcessOrder(new Order { Side = OrderSide.Buy, Price = 100, Quantity = 10 });
 
-        orderBook.Print();
-        orderBook.GetBestBuy();
-        orderBook.GetBestSell();
+        foreach (var trade in buyTrades)
+        {
+            Console.WriteLine($"TRADE: {trade.Quantity} @ {trade.Price}");
+        }
 
     }
 }
