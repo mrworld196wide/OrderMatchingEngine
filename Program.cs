6cf3c55c722c467a48efa6cefef21a9861b9a09f
Aishwar Pathak <shubhum19@gmail.com>
2026-05-05 15:08:02 +0530

    cancel  check

--- Program.cs
+++ Program.cs
@@ -44,5 +44,14 @@ class Program
         Process(new Order { Side = OrderSide.Sell, Price = 102, Quantity = 45 });
 
         Console.WriteLine("\nDone");
+
+        var b3 = new Order { Side = OrderSide.Buy, Price = 90, Quantity = 5 };
+        var b4 = new Order { Side = OrderSide.Buy, Price = 91, Quantity = 5 };
+        Console.WriteLine("\nCancel Order:");
+        orderBook.CancelOrder(b4.Id);
+        orderBook.Print();
+        Console.WriteLine("\n Modify Order:");
+        orderBook.ModifyOrder(b3.Id, 108, 7);
+        orderBook.Print();
     }
 }
